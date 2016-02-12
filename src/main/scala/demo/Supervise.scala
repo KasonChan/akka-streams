b264c4135ec11d9119303d5c1deb1131e58e7a13
kasonchan <kasonl.chan@gmail.com>
2016-02-12 14:53:31 +0800

    Add Supervise

--- src/main/scala/demo/Supervise.scala
+++ src/main/scala/demo/Supervise.scala
@@ -4,32 +4,28 @@ import akka.actor.ActorSystem
 import akka.stream.scaladsl.{Sink, Source}
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings, Supervision}
 
-import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
+import scala.util.{Failure, Success}
 
 /**
-  * Created by ka-son on 8/2/15.
+  * Created by kasonchan on 2/12/16.
   */
 object Supervise extends App {
 
-  implicit val system = ActorSystem("system")
-
   val decider: Supervision.Decider = {
     case _: ArithmeticException => Supervision.Resume
-    case _ => Supervision.Stop
+    case _ => Supervision.Restart
   }
 
-  // Create materializer with a custom supervision strategy
-  implicit val mat = ActorMaterializer(
-    ActorMaterializerSettings(system).withSupervisionStrategy(decider))
+  implicit val system = ActorSystem("system")
+  implicit val materializer = ActorMaterializer(ActorMaterializerSettings(system).withSupervisionStrategy(decider))
 
   val source = Source(0 to 5).map(100 / _)
-  val result = source.runWith(Sink.fold(0)(_ + _))
-
-  val res = Await.result(result, Duration.Inf)
-  println(res)
-  result.map(r => println(r))
-  system.shutdown()
+  val result = source.runWith(Sink.fold(0)(_ + _)).onComplete {
+    case Success(s) => println(s)
+      system.shutdown()
+    case Failure(f) => println(f)
+      system.shutdown()
+  }
 
 }
